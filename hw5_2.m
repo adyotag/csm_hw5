a2d741b8cd644c86a7fd13a462cb201a114cebcf
Adyota Gupta <adyotag@Thorium.(none)>
2020-05-05 01:55:23 -0400

    Fixed bug with total vs elastic strain for trial stress state; added comments; FINAL VERSION

--- hw5_2.m
+++ hw5_2.m
@@ -11,15 +11,15 @@ global mu; mu = 30.38E3;        % in MPa
 delta_t = 0.001; end_time = 4.;
 global times; times = 0:delta_t:end_time;
 
-eps_p_hist =  zeros(length(times)+1,3,3);
-q_hist =  zeros(length(times)+1,3,3);
-alpha_hist =  zeros(length(times)+1,1);
-stress_hist = zeros(length(times)+1,3,3);
-strain_hist = zeros(length(times)+1,3,3);
+eps_p_hist =  zeros(length(times)+1,3,3);   % stores plastic strain history
+q_hist =  zeros(length(times)+1,3,3);       % back stress history
+alpha_hist =  zeros(length(times)+1,1);     % cumulative plastic strain history
+stress_hist = zeros(length(times)+1,3,3);   % stress history
+strain_hist = zeros(length(times)+1,3,3);   % stores elastic strain history
 
 % Perform return mapping algorithm
 for i = 1:length(times)
-    strain_hist(i+1,:,:) = nextStep(i);  % obtain strain at next step
+    strain_hist(i+1,:,:) = nextStep(i, squeeze(eps_p_hist(i,:,:)) );  % obtain strain at next step
     delta_strain = squeeze(strain_hist(i+1,:,:) - strain_hist(i,:,:));
     delta_stress = zeros(3); delta_stress(2,2) = E/(1-nu^2) * delta_strain(2,2);
     delta_stress(3,3) = E*nu/(1-nu^2) * delta_strain(2,2);
@@ -36,8 +36,8 @@ for i = 1:length(times)
         alpha_hist(i+1) = alpha_hist(i);
         
     else        % Plastic case
-        next_n = next_ksi_tr/mag(next_ksi_tr);
-        delta_gamma = next_yF_tr/(K+H+2*mu);
+        next_n = next_ksi_tr/mag(next_ksi_tr);  % Updates normal tensor
+        delta_gamma = next_yF_tr/(K+H+2*mu);    % Computes the plastic strain multiplier
                 
         stress_hist(i+1,:,:) = squeeze(stress_hist(i+1,:,:)) - 2*mu*delta_gamma*next_n;    % Updates stress at next time step  
         eps_p_hist(i+1,:,:) = delta_gamma*next_n + squeeze(eps_p_hist(i,:,:));             % Updates plastic strain at next time step
@@ -51,28 +51,28 @@ end
 
 % Plots
 figure;
-plot(strain_hist(:,1,1),stress_hist(:,1,1)); hold on
-plot(strain_hist(:,2,2),stress_hist(:,2,2));
-plot(strain_hist(:,3,3),stress_hist(:,3,3));
+plot(strain_hist(:,1,1)+eps_p_hist(:,1,1),stress_hist(:,1,1)); hold on
+plot(strain_hist(:,2,2)+eps_p_hist(:,2,2),stress_hist(:,2,2));
+plot(strain_hist(:,3,3)+eps_p_hist(:,3,3),stress_hist(:,3,3));
 legend('$$\sigma_{11}$$', '$$\sigma_{22}$$', '$$\sigma_{33}$$', 'interpreter', 'latex');
 title('Cyclic Stress-Strain Response under Plane Strain Conditions');
 ylabel('Stress, $$\sigma$$ (MPa)', 'interpreter', 'latex');
 xlabel('Strain, $$\epsilon$$', 'interpreter', 'latex');
 
 figure;
-plot(strain_hist(:,2,2),strain_hist(:,1,1));
-title('Lateral Strain against Axial Strain');
+plot(strain_hist(:,2,2)+eps_p_hist(:,2,2),strain_hist(:,1,1)+eps_p_hist(:,1,1));
+title('Lateral Plastic Strain against Axial Strain');
 ylabel('Lateral Strain, $$\epsilon_{11}$$', 'interpreter', 'latex');
 xlabel('Axial Strain, $$\epsilon_{22}$$', 'interpreter', 'latex');
 
 figure;
-plot(strain_hist(:,2,2), eps_p_hist(:,2,2));
+plot(strain_hist(:,2,2)+eps_p_hist(:,2,2), eps_p_hist(:,2,2));
 title('Axial Plastic Strain against Total Axial Strain');
 ylabel('Strain, $$\epsilon_{22}^p$$', 'interpreter', 'latex');
 xlabel('Total Axial Strain, $$\epsilon_{22}$$', 'interpreter', 'latex');
 
 figure;
-plot(strain_hist(:,2,2), alpha_hist);
+plot(strain_hist(:,2,2)+eps_p_hist(:,2,2), alpha_hist);
 title('Cumulative Plastic Strain against Total Axial Strain');
 ylabel('Cumulative Plastic Strain, $$\alpha$$', 'interpreter', 'latex');
 xlabel('Total Axial Strain, $$\epsilon_{22}$$', 'interpreter', 'latex');
@@ -81,10 +81,10 @@ xlabel('Total Axial Strain, $$\epsilon_{22}$$', 'interpreter', 'latex');
 %%%%%%%%%%% RELEVANT FUNCTIONS %%%%%%%%%%%
 
 % Imposes plane strain conditions, and outputs strain at next time step
-function r = nextStep(i) % Strain, stress
+function r = nextStep(i, eps_p) % Strain
     global nu; global times;
     r = zeros(3); t = times(i);   
-    r(2,2) = 0.01*sin(2*pi*t);  % Computes new strain
+    r(2,2) = 0.01*sin(2*pi*t) - eps_p(2,2);  % Computes new strain; elastic portion
     r(1,1) = -nu/(1-nu) * r(2,2);
 end
 
